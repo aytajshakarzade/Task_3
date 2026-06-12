0857e64423559784f643bfec75db9e5b9ed6db6a
aytajshakarzade <ayitajshakarzade@gmail.com>
2026-06-12 18:03:56 +0400

    second

--- Program.cs
+++ Program.cs
@@ -9,8 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString(
-            "DefaultConnection"));
+        builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddScoped<CityRepository>();
@@ -20,13 +19,12 @@ builder.Services.AddScoped<HourlyRepository>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("Frontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -35,13 +33,12 @@ builder.Services.AddSwaggerGen(options =>
 {
     options.EnableAnnotations();
 
-    options.SwaggerDoc("v1",
-        new OpenApiInfo
-        {
-            Title = "Weather API",
-            Version = "v1",
-            Description = "Weather information service"
-        });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Weather API",
+        Version = "v1",
+        Description = "Weather information service"
+    });
 });
 
 var app = builder.Build();
